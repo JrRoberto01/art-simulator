1edfa29ffefb38abc912322a165ed243247d5812
Roberto Jr <contatorj.roberto@gmail.com>
2024-04-29 17:42:12 -0300

    upload novos arquivos e versão PHP

--- _script/script.js
+++ _script/script.js
@@ -3,18 +3,3 @@ let refNumber = '123'
 function initialConfig(){
     document.getElementById('refNumber').innerText = (`Nº ${refNumber}`)
 }
-
-initialConfig();
-//Gerador PDF
-function downloadPDF() {
-    const item = document.querySelector(".Content");
-  
-    var options = {
-      margin: [10,10,10,10],
-      filename: "art.pdf",
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: "mm", format: "letter", orientation: "portrait" },
-    };
-  
-    html2pdf().set(options).from(item).save();
-  }

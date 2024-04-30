3189690cd00a7f2c387092e52f41365a161ba873
Roberto Jr <contatorj.roberto@gmail.com>
2024-04-29 22:41:25 -0300

    adicionando campos

--- _script/script.js
+++ _script/script.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 let refNumber = '123'
 
 function initialConfig(){
     document.getElementById('refNumber').innerText = (`Nº ${refNumber}`)
-=======
-let refNumber = '123'
-
-function initialConfig(){
-    document.getElementById('refNumber').innerText = (`Nº ${refNumber}`)
->>>>>>> bed309b4330dc5e9fb495c2e74eea7b3526de245
 }
+
+initialConfig();

5784253e8c95571fd64f401f7ea3be56fa8df4fa
Roberto Jr <contatorj.roberto@gmail.com>
2024-05-02 16:53:50 -0300

    update

--- art-generator.php
+++ art-generator.php
@@ -4,7 +4,8 @@ require __DIR__ . "/vendor/autoload.php";
 use Dompdf\Dompdf;
 use Dompdf\options;
 
-//$html = "<h1>$name</h1>";
+//Variaveis
+$nome_profissional = $_POST["nome-profissional"];
 
 $options = new Options;
 $options-> setChroot(__DIR__);
@@ -15,7 +16,11 @@ $dompdf-> setPaper("A4");
 $dompdf-> addInfo("Title", "PDF ART Simulado");
 $dompdf-> addInfo("Author", "Simulador ART UNIVASSOURAS");
 $html = file_get_contents("art-template.html");
+
+//Replaces
+$html = str_replace(["{{ nome-profissional }}"], [$nome_profissional], $html);
 $dompdf-> loadHtml($html);
+
 //$dompdf-> loadHtmlFile("art-template.html");
 $dompdf -> render();
 $dompdf -> stream("art-document.pdf", ["Attachment" => 0]);

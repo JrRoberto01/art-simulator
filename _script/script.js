75048870d6fdc21395134fe6a4865406400514a3
Roberto Jr <contatorj.roberto@gmail.com>
2024-06-10 23:37:31 -0300

    Adicionando arquivos novos

--- _script/script.js
+++ _script/script.js
@@ -1,131 +1,144 @@
-let refNumber = '123'
+let len = 9;
+let refNumber = parseInt((Math.random() * 9 + 1) * Math.pow(10,len-1), 10);
+const radioButtons = document.querySelectorAll('input[name="forma-registro"]');
+const radioButtonsSec = document.querySelectorAll('input[name="part-tecnica"]');
+const radioButtonsThi = document.querySelectorAll('input[name="tipo-registro"]');
+const artPrincipalInput = document.getElementById('art-principal');
+const artPartTec = document.getElementById('art-part-tec');
+const modalInfosAtv = document.getElementById('modal-infos');
+const closeBtn = document.getElementById('close-btn');
 
-var lista_ent_classe = [
+const lista_ent_classe = [
     "ABENC – Associação Brasileira de Engenheiros Civis",
     "AEVR – Associação de Engenheiros de Volta Redonda",
     "ASSEA – Associação de Engenheiros e Arquitetos de Três Rios e Região",
     "CLUBE – Clube de Engenharia",
     "SENGE–RJ – Sindicato dos Engenheiros do Estado do Rio de Janeiro",
     "SENGE–VR – Sindicato dos Engenheiros de Volta Redonda",
-    "NENHUMA"];
-var ramo = [
-    "Engenharia Civil",
-    "Engenharia Industrial Mod. Civil"];
-var atividade = [
-    "Análise",
-    "Assessoria",
-    "Assistência",
-    "Avaliação",
-    "Condução de Equipe de Instalação",
-    "Condução de Equipe de Manutenção",
-    "Condução de Equipe de Montagem",
-    "Condução de Trabalho Técnico",
-    "Coordenação Técnica",
-    "Desempenho de Cargo Técnico",
-    "Desempenho de Função Técnica",
-    "Elaboração de Orçamento",
-    "Especificação",
-    "Estudo de Viabilidade Técnico-Econômica",
-    "Execução de Desenho Técnico",
-    "Execução de Instalação",
-    "Execução de Montagem",
-    "Execução de Obra",
-    "Execução de Reparo",
-    "Execução de Serviço Técnico",
-    "Fiscalização de Obra",
-    "Fiscalização de Serviço Técnico",
-    "Laudo Técnico",
-    "Manutenção de Equipamento",
-    "Manutenção de Instalação",
-    "Mensuração",
-    "Orientação Técnica",
-    "Perícia",
-    "Pesquisa",
-    "Planejamento",
-    "Produção Técnica Especializada",
-    "Projeto",
-    "Responsável Técnico por Empresa",
-    "Supervisão Técnica",
-    "Vistoria"];
-var especificacao_atividade= [
-    "Aferição",
-    "Aplicação",
-    "Calçamento",
-    "Cálculo",
-    "Classificação",
-    "Concretagem",
-    "Conservação",
-    "Construção",
-    "Contenção",
-    "Demolição",
-    "Desmembramento",
-    "Desmonte",
-    "Detalhamento",
-    "Dimensionamento",
-    "Dragagem",
-    "Drenagem",
-    "Escoramento",
-    "Estaqueamento",
-    "Fundação",
-    "Gerência",
-    "Legalização",
-    "Limpeza",
-    "Loteamento",
-    "Melhoramento",
-    "Modificação",
-    "Nivelamento",
-    "Pavimentação",
-    "Reforma",
-    "Remembramento",
-    "Restauração",
-    "Saneamento",
-    "Sondagem",
-    "Terraplenagem",
-    "Transformação"];
-var complemento = [
-    "Aeroporto",
-    "Aerofotogrametria",
-    "Alvenaria",
-    "Andaime",
-    "Aqueduto",
-    "Arquitetura",
-    "Barragem",
-    "Canal",
-    "Central de Gás",
-    "Cimento",
-    "Concreto",
-    "Edificação Residencial",
-    "Edificação Comercial",
-    "Encosta",
-    "Escola",
-    "Estação de Tratamento",
-    "Estrutura de Concreto Armado",
-    "Estrada",
-    "Estrutura",
-    "Ferrovia",
-    "Laje",
-    "Lay-out",
-    "Galeria",
-    "Galpão",
-    "Gleba",
-    "Hidrossanitária",
-    "Hospital",
-    "Moradia Econômica",
-    "Passarela",
-    "Piscina",
-    "Praça",
-    "Rede Elétrica",
-    "Rede de Esgoto",
-    "Rede de Gás",
-    "Rede Hidráulica",
-    "Rodovia",
-    "Sinalização",
-    "Sistema de Abastecimento D'água",
-    "Topografia",
-    "Viabilidade",
-    "Outros"];
+    "NENHUMA"
+];
+const ramo = [
+    "1101 - Engenharia Civil",
+    "1109 - Engenharia Industrial Mod. Civil"
+];      
+const atividade = [
+    { nome: "Análise", codigo: '1' },
+    { nome: "Assessoria", codigo: '3' },
+    { nome: "Assistência", codigo: '4' },
+    { nome: "Avaliação", codigo: '5' },
+    { nome: "Condução de Equipe de Instalação", codigo: '6' },
+    { nome: "Condução de Equipe de Manutenção", codigo: '7' },
+    { nome: "Condução de Equipe de Montagem", codigo: '8' },
+    { nome: "Condução de Trabalho Técnico", codigo: '11' },
+    { nome: "Coordenação Técnica", codigo: '14' },
+    { nome: "Desempenho de Cargo Técnico", codigo: '15' },
+    { nome: "Desempenho de Função Técnica", codigo: '16' },
+    { nome: "Elaboração de Orçamento", codigo: '20' },
+    { nome: "Especificação", codigo: '23' },
+    { nome: "Estudo de Viabilidade Técnico-Econômica", codigo: '25' },
+    { nome: "Execução de Desenho Técnico", codigo: '26' },
+    { nome: "Execução de Instalação", codigo: '27' },
+    { nome: "Execução de Montagem", codigo: '28' },
+    { nome: "Execução de Obra", codigo: '29' },
+    { nome: "Execução de Reparo", codigo: '30' },
+    { nome: "Execução de Serviço Técnico", codigo: '31' },
+    { nome: "Fiscalização de Obra", codigo: '34' },
+    { nome: "Fiscalização de Serviço Técnico", codigo: '35' },
+    { nome: "Laudo Técnico", codigo: '36' },
+    { nome: "Manutenção de Equipamento", codigo: '37' },
+    { nome: "Manutenção de Instalação", codigo: '38' },
+    { nome: "Mensuração", codigo: '39' },
+    { nome: "Orientação Técnica", codigo: '42' },
+    { nome: "Perícia", codigo: '45' },
+    { nome: "Pesquisa", codigo: '46' },
+    { nome: "Planejamento", codigo: '47' },
+    { nome: "Produção Técnica Especializada", codigo: '48' },
+    { nome: "Projeto", codigo: '49' },
+    { nome: "Responsável Técnico por Empresa", codigo: '68' },
+    { nome: "Supervisão Técnica", codigo: '50' },
+    { nome: "Vistoria", codigo: '51' }
+];
+const especificacao_atividade = [
+    { nome: "Aferição", codigo: "01" },
+    { nome: "Aplicação", codigo: "02" },
+    { nome: "Calçamento", codigo: "06" },
+    { nome: "Cálculo", codigo: "07" },
+    { nome: "Classificação", codigo: "10" },
+    { nome: "Concretagem", codigo: "11" },
+    { nome: "Conservação", codigo: "12" },
+    { nome: "Construção", codigo: "13" },
+    { nome: "Contenção", codigo: "14" },
+    { nome: "Demolição", codigo: "16" },
+    { nome: "Desmembramento", codigo: "17" },
+    { nome: "Desmonte", codigo: "18" },
+    { nome: "Detalhamento", codigo: "19" },
+    { nome: "Dimensionamento", codigo: "20" },
+    { nome: "Dragagem", codigo: "21" },
+    { nome: "Drenagem", codigo: "22" },
+    { nome: "Escoramento", codigo: "23" },
+    { nome: "Estaqueamento", codigo: "25" },
+    { nome: "Fundação", codigo: "31" },
+    { nome: "Gerência", codigo: "32" },
+    { nome: "Legalização", codigo: "37" },
+    { nome: "Limpeza", codigo: "38" },
+    { nome: "Loteamento", codigo: "40" },
+    { nome: "Melhoramento", codigo: "42" },
+    { nome: "Modificação", codigo: "43" },
+    { nome: "Nivelamento", codigo: "45" },
+    { nome: "Pavimentação", codigo: "47" },
+    { nome: "Reforma", codigo: "60" },
+    { nome: "Remembramento", codigo: "61" },
+    { nome: "Restauração", codigo: "62" },
+    { nome: "Saneamento", codigo: "64" },
+    { nome: "Sondagem", codigo: "65" },
+    { nome: "Terraplenagem", codigo: "66" },
+    { nome: "Transformação", codigo: "68" }
+];
+const complemento = [
+  { nome: "Aeroporto", codigo: "03" },
+  { nome: "Aerofotogrametria", codigo: "04" },
+  { nome: "Alvenaria", codigo: "08" },
+  { nome: "Andaime", codigo: "09" },
+  { nome: "Aqueduto", codigo: "12" },
+  { nome: "Arquitetura", codigo: "13" },
+  { nome: "Barragem", codigo: "15" },
+  { nome: "Canal", codigo: "24" },
+  { nome: "Central de Gás", codigo: "28" },
+  { nome: "Cimento", codigo: "31" },
+  { nome: "Concreto", codigo: "34" },
+  { nome: "Edificação Residencial", codigo: "40" },
+  { nome: "Edificação Comercial", codigo: "41" },
+  { nome: "Encosta", codigo: "47" },
+  { nome: "Escola", codigo: "58" },
+  { nome: "Estação de Tratamento", codigo: "61" },
+  { nome: "Estrutura de Concreto Armado", codigo: "64" },
+  { nome: "Estrada", codigo: "66" },
+  { nome: "Estrutura", codigo: "67" },
+  { nome: "Ferrovia", codigo: "71" },
+  { nome: "Laje", codigo: "91" },
+  { nome: "Lay-out", codigo: "92" },
+  { nome: "Galeria", codigo: "75" },
+  { nome: "Galpão", codigo: "76" },
+  { nome: "Gleba", codigo: "81" },
+  { nome: "Hidrossanitária", codigo: "83" },
+  { nome: "Hospital", codigo: "84" },
+  { nome: "Moradia Econômica", codigo: "100" },
+  { nome: "Passarela", codigo: "108" },
+  { nome: "Piscina", codigo: "110" },
+  { nome: "Praça", codigo: "124" },
+  { nome: "Rede Elétrica", codigo: "128" },
+  { nome: "Rede de Esgoto", codigo: "129" },
+  { nome: "Rede de Gás", codigo: "130" },
+  { nome: "Rede Hidráulica", codigo: "131" },
+  { nome: "Rodovia", codigo: "141" },
+  { nome: "Sinalização", codigo: "143" },
+  { nome: "Sistema de Abastecimento D'água", codigo: "144" },
+  { nome: "Topografia", codigo: "166" },
+  { nome: "Viabilidade", codigo: "173" },
+  { nome: "Outros", codigo: "17" }
+];
 
-/*function preencherDropdown(dropdown, lista) {
+function preencherDropdown(dropdown, lista) {
     var select = document.getElementById(dropdown);
     lista.forEach(function(item) {
         var option = document.createElement("option");
@@ -136,12 +149,82 @@ var complemento = [
 
 preencherDropdown("entidade-classe", lista_ent_classe);
 preencherDropdown("ramo", ramo);
-preencherDropdown("atividade", atividade);
+/*preencherDropdown("atividade", atividade);
 preencherDropdown("espec-atv", especificacao_atividade);
 preencherDropdown("compl-atv", complemento);*/
 
 function initialConfig(){
-    document.getElementById('refNumber').innerText = (`Nº ${refNumber}`)
+    document.getElementById('refNumber').innerHTML = (`Nº 2024${refNumber}`);
+    //Mostrar/Esconder Campos text
+    radioButtons.forEach(radioButton => {
+        radioButton.addEventListener('change', () => {
+          if (radioButton.value === 'Inicial') {
+            artPrincipalInput.style.display = 'none';
+          } else {
+            artPrincipalInput.style.display = 'block';
+          }
+        });
+      });
+      radioButtonsSec.forEach(radioButtonsSec => {
+        radioButtonsSec.addEventListener('change', () => {
+          if (radioButtonsSec.value === 'Individual') {
+            artPartTec.style.display = 'none';
+          } else {
+            artPartTec.style.display = 'block';
+          }
+        });
+      });
+      radioButtonsThi.forEach(radioButtonsThi => {
+        radioButtonsThi.addEventListener('change', () => {
+          switch (radioButtonsThi.value) {
+            case 'Obra ou Serviço':
+              document.getElementById('campo-valor-salario').style.display = 'none';
+              document.getElementById('campo-valor-contrato').style.display = 'block';
+              document.getElementById('campo-valor-salario').style.visibility = 'hidden';
+              document.getElementById('campo-valor-contrato').style.visibility = 'visible';
+              break;
+            case 'Desempenho de Cargo ou função':
+              document.getElementById('campo-valor-salario').style.display = 'block';
+              document.getElementById('campo-valor-contrato').style.display = 'none';
+              document.getElementById('campo-valor-salario').style.visibility = 'visible';
+              document.getElementById('campo-valor-contrato').style.visibility = 'hidden';
+              break;
+            case 'Múltipla':
+              document.getElementById('campo-valor-salario').style.display = 'none';
+              document.getElementById('campo-valor-contrato').style.display = 'block';
+              document.getElementById('campo-valor-salario').style.visibility = 'hidden';
+              document.getElementById('campo-valor-contrato').style.visibility = 'visible';
+            default:
+              break;
+          }
+        });
+      });
+
+}
+//Modal Listagem
+function listagemAtividades(matriz){
+  const ul = document.createElement('ul');
+
+  matriz.forEach(item =>{
+    const li = document.createElement('li');
+      li.textContent = item.codigo + " - " + item.nome;
+      ul.appendChild(li);
+  });
+  return ul;
 }
 
+closeBtn.addEventListener('click', () => {
+  modalInfosAtv.style.visibility = 'hidden';
+});
+
+function abrirModalAtv(){
+  if (modalInfosAtv.style.visibility === 'hidden') {
+    modalInfosAtv.style.visibility = 'visible';
+  }else{
+    modalInfosAtv.style.visibility = 'hidden';
+  }
+}
+
+const preencherLista = document.getElementById('modal-content');
+preencherLista.appendChild(listagemAtividades(atividade));
 initialConfig();
--- art-generator.php
+++ art-generator.php
@@ -6,6 +6,31 @@ use Dompdf\options;
 
 //Variaveis
 $nome_profissional = $_POST["nome-profissional"];
+$reg_profissional = $_POST["reg-profissional"];
+$nome_empresa = $_POST["nome-empresa"];
+$num_reg_emp = $_POST["num-reg-emp"];
+$nome_contratante = $_POST["nome-contratante"];
+$id_contratante = $_POST["id-contratante"];
+$compl_contratante = $_POST["compl-contratante"];
+$bairro_contratante = $_POST["bairro-contratante"];
+$num_contratante = $_POST["num-contratante"];
+$cidade_contratante = $_POST["cidade-contratante"];
+$uf_contratante = $_POST["uf-contratante"];
+$cep_contratante = $_POST["cep-contratante"];
+$num_contrato = $_POST["num-contrato"];
+$data_art = $_POST["data-art"];
+$valor_contrato = $_POST["valor-contrato"];
+$end_contrato = $_POST["end-contrato"];
+$compl_contrato = $_POST["compl-contrato"];
+$bairro_contrato = $_POST["bairro-contrato"];
+$end_num_contrato = $_POST["end-num-contrato"];
+$cidade_contrato = $_POST["cidade-contrato"];
+$uf_contrato = $_POST["uf-contrato"];
+$cep_contrato = $_POST["cep-contrato"];
+$data_inicio = $_POST["data-inicio"];
+$prazo_contrato = $_POST["prazo-contrato"];
+$nome_contratante = $_POST["nome-contratante"];
+$descricao = $_POST["descricao"];
 
 $options = new Options;
 $options-> setChroot(__DIR__);
@@ -18,7 +43,60 @@ $dompdf-> addInfo("Author", "Simulador ART UNIVASSOURAS");
 $html = file_get_contents("art-template.html");
 
 //Replaces
-$html = str_replace(["{{ nome-profissional }}"], [$nome_profissional], $html);
+$html = str_replace([
+    "{{ nome-profissional }}", 
+    "{{ reg-profissional }}",
+    "{{ nome-empresa }}",
+    "{{ num-reg-emp }}",
+    "{{ nome-contratante }}",
+    "{{ id-contratante }}",
+    "{{ compl-contratante }}",
+    "{{ bairro-contratante }}",
+    "{{ num-contratante }}",
+    "{{ cidade-contratante }}",
+    "{{ uf-contratante }}",
+    "{{ cep-contratante }}",
+    "{{ num-contrato }}",
+    "{{ data-art }}",
+    "{{ valor-contrato }}",
+    "{{ end-contrato }}",
+    "{{ compl-contrato }}",
+    "{{ bairro-contrato }}",
+    "{{ end-num-contrato }}",
+    "{{ cidade-contrato }}",
+    "{{ uf-contrato }}",
+    "{{ cep-contrato }}",
+    "{{ data-inicio }}",
+    "{{ prazo-contrato }}",
+    "{{ descricao }}"
+    ], 
+    [
+    $nome_profissional,
+    $reg_profissional,
+    $nome_empresa,
+    $num_reg_emp,
+    $nome_contratante,
+    $id_contratante,
+    $compl_contratante,
+    $bairro_contratante,
+    $num_contratante,
+    $cidade_contratante,
+    $uf_contratante,
+    $cep_contratante,
+    $num_contrato,
+    $data_art,
+    $valor_contrato,
+    $end_contrato,
+    $compl_contrato,
+    $bairro_contrato,
+    $end_num_contrato,
+    $cidade_contrato,
+    $uf_contrato,
+    $cep_contrato,
+    $data_inicio,
+    $prazo_contrato,
+    $descricao],
+    $html);
 $dompdf-> loadHtml($html);
 
 //$dompdf-> loadHtmlFile("art-template.html");
